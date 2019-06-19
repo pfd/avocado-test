232a15579870b613f6e47992bcb5ccbfd246928e
Pete Davis <pete@urbanairship.com>
2019-06-19 14:00:09 -0700

    add push worker

---
+++ push-worker.js
@@ -0,0 +1,11 @@
+// 86acbd31cd7c09cf30acb66d2fbedc91daa48b86:1560977510.66
+importScripts('https://aswpsdkus.com/notify/v1/ua-sdk.min.js')
+uaSetup.worker(self, {
+  defaultIcon: 'https://avocadotest.com/images/toast.png',
+  defaultTitle: 'Avocado Riposte',
+  defaultActionURL: 'https://avocadotest.com',
+  appKey: 'dbYFSjwBQrWnThKEO1fOIQ',
+  token: 'MTpkYllGU2p3QlFyV25UaEtFTzFmT0lROkliVmRIVk5PTVB1X2hZLXRxc0pNNG8yTkdHV0hhVmpkUkdzaGl0ZF9ROW8',
+  vapidPublicKey: 'BImN3k4ecgowM8-WEB3eZCLYMYbHm82TNUeBjgpwH-yL7ZVILAj_4WQYgPuZLbJn9-6-lgxGDRgmFjnvKAI548M='
+})
+
