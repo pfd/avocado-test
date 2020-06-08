e2f29adb182faa4c51c95c17d6b8fbd8bb1524aa
pfd <davispete@gmail.com>
2020-06-07 22:39:12 -0700

    more algolia ui

---
+++ content/en/search.md
@@ -0,0 +1,8 @@
+---
+title: Search Results
+layout: search
+---
+
+<div id="hits">
+  <!-- Hits widget will appear here -->
+</div>
--- static/javascripts/algolia.js
+++ static/javascripts/algolia.js
@@ -28,3 +28,13 @@ search.addWidget(
       },
     })
   );
+
+  // create variable for custom hit template
+var hitTemplate =
+'<a href="{{ permalink }}" class="List-item">' +
+'<div class="List-image">' +
+'<img src="https://res.cloudinary.com/harrycresswell/image/upload/w_auto,dpr_auto,c_scale/{{{featuredimage}}}" />' +
+"</div>" +
+'<div class="List-title">{{{_highlightResult.title.value}}}</div>' +
+"</a>" +
+'<div class="List-summary">{{{summary}}}</div>';
